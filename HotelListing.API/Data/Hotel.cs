f6c6b98e30273a44fccb62677b4b30042786e742
maloney_f <missymaloney1@gmail.com>
2022-10-26 18:01:38 -0400

    Added EntityFramework and Created Database

---
+++ HotelListing.API/Data/Hotel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Metrics;
+
+namespace HotelListing.API.Data
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating { get; set; }
+
+        [ForeignKey(nameof(CountryId))]
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+    }
+}
